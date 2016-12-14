d6b0a6da33275588ded65e00152161438db75d5a
Stephanie Zeng <stephaniezeng521@gmail.com>
2016-12-13 23:32:38 -0500

    Made ajax call and built badges.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,15 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url:'https://www.codeschool.com/users/stephanie56.json',
+    dataType:'jsonp',
+    success:function(data){
+      var courseInfo = data.courses.completed;
+      courseInfo.forEach(function(course){
+        $("#badges").append("<div class='course'><h3>" +course.title +"</h3><img src='"+ course.badge +"' /><a class='btn btn-primary' href='"+ course.url +"' target='_blank'>See Course</a></div>");
+      });
+    }
+  });
 
 });
